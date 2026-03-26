25bdd009753e9e525e21b26d9517c22ab88383ef
Filippos Dimitriou <AQ241177@aquinas.ac.uk>
2026-03-26 10:23:10 +0000

    Added alt text button and started writing javascript for it

---
+++ js/ToggleAltDisplay.js
@@ -0,0 +1,6 @@
+function toggleAltDisplay() {
+	const CustomStyle = getComputedStyle(document.documentElement);
+	const Blue = rootStyle.getPropertyValue("--dark-blue");
+    const BackgroundGrey = rootStyle.getPropertyValue("--background-grey");
+	const 
+}
